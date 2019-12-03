a2bb1df88cf9d58f7e1604e0a8ca0981f62595e4
Alisson <alissonlucas1821@gmail.com>
2019-12-03 17:53:10 -0300

    Uri 1047 não válido, a fazer

---
+++ Execs/Calcs/Uri1045.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Globalization;
+
+namespace Execs
+{
+  class Uri1045
+  {
+
+    public static void TrianguloTypes()
+    {
+      string[] valores = Console.ReadLine().Split(' ');
+      double n1, n2, n3, A, B, C;
+      n1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+      n2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+      n3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+      if (n1 > n2 && n1 > n3)
+      {
+        A = n1;
+        if (n2 > n3)
+        {
+          B = n2;
+          C = n3;
+        }
+        else
+        {
+          B = n3;
+          C = n2;
+        }
+      }
+      else if (n2 > n3)
+      {
+        A = n2;
+        if (n1 > n3)
+        {
+          B = n1;
+          C = n3;
+        }
+        else
+        {
+          B = n3;
+          C = n1;
+        }
+      }
+      else
+      {
+        A = n3;
+        if (n1 > n2)
+        {
+          B = n1;
+          C = n2;
+        }
+        else
+        {
+          B = n2;
+          C = n1;
+        }
+      }
+
+      if (A >= (B + C))
+      {
+        Console.WriteLine("NAO FORMA TRIANGULO");
+      }
+      else
+      {
+        if (Math.Pow(A, 2) == (Math.Pow(B, 2) + Math.Pow(C, 2)))
+        {
+          Console.WriteLine("TRIANGULO RETANGULO");
+        }
+        else if (Math.Pow(A, 2) > (Math.Pow(B, 2) + Math.Pow(C, 2)))
+        {
+          Console.WriteLine("TRIANGULO OBTUSANGULO");
+        }
+        else
+        {
+          Console.WriteLine("TRIANGULO ACUTANGULO");
+        }
+        if (A == B && B == C)
+        {
+          Console.WriteLine("TRIANGULO EQUILATERO");
+        }
+        else if (A == B || A == C || B == C)
+        {
+          Console.WriteLine("TRIANGULO ISOSCELES");
+        }
+      }
+    }
+  }
+}
