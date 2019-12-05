462b71ad54a9e8b59327d02942f7b4def7281380
Alisson <alissonlucas1821@gmail.com>
2019-12-05 17:56:33 -0300

    Uri1051 incompleto

--- Execs/Program.cs
+++ Execs/Program.cs
@@ -8,7 +8,30 @@ namespace Execs
 
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      double salario, imposto;
+      salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      if (salario <= 2000.00)
+      {
+        Console.WriteLine("Isento");
+      }
+      else if (salario > 2000.00 && salario <= 3000.00)
+      {
+        imposto = salario * 0.08;
+        Console.WriteLine("R$" + imposto);
+      }
+      else if (salario > 3000.00 && salario <= 4500.00)
+      {
+        var after3000 = salario - 3000.00;
+        imposto = (1000.00 * 0.08) + (after3000 * 0.18);
+        Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+      }
+      else
+      {
+        var after4500 = salario - 4500.00;
+        imposto = (2500.00 * 0.08) + (after4500 * 0.28);
+        Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+      }
+
     }
   }
 }
