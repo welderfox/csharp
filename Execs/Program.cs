01d55a6cf22f6d4e58721e85c3aa2def943c6599
Alisson <alissonlucas1821@gmail.com>
2019-12-06 14:17:48 -0300

    Limpeza do Program.cs

--- Execs/Program.cs
+++ Execs/Program.cs
@@ -8,30 +8,7 @@ namespace Execs
 
     public static void Main(string[] args)
     {
-      double salario, imposto;
-      salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-      if (salario <= 2000.00)
-      {
-        Console.WriteLine("Isento");
-      }
-      else if (salario > 2000.00 && salario <= 3000.00)
-      {
-        var after2000 = salario - 2000.00;
-        imposto = after2000 * 0.08;
-        Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
-      }
-      else if (salario > 3000.00 && salario <= 4500.00)
-      {
-        var after3000 = salario - 3000.00;
-        imposto = (1000.00 * 0.08) + (after3000 * 0.18);
-        Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
-      }
-      else
-      {
-        var after4500 = salario - 4500.00;
-        imposto = (1000.00 * 0.08) + (after4500 * 0.28) + (1500.00 * 0.18);
-        Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
-      }
+      Console.WriteLine("Hello World!");
     }
   }
 }
