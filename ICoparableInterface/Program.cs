5c440dd3a32702da18272ce2c27e831bdedcaa20
Alisson <alissonlucas1821@gmail.com>
2020-10-25 13:33:51 -0300

    Aplicando IComparable em uma classe customizada

---
+++ ICoparableInterface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ICoparableInterface.Entities;
+
+namespace ICoparableInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/home/alisson/Projetos/Git/in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
